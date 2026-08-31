6a5c3487e918fd0a1191b3435dfde9f92ed8f8be
lsc110 <614709442@qq.com>
2026-08-31 15:12:50 +0800

    项目主体

--- neural-core/src/test/java/org/micro/neural/extension/prototype/NpiPrototype.java
+++
@@ -1,8 +0,0 @@
-package org.micro.neural.extension.prototype;
-
-import org.micro.neural.extension.SPI;
-
-@SPI
-public interface NpiPrototype {
-    long spiHello();
-}
--- neural-core/src/test/java/org/micro/neural/extension/singleton/NpiSingleton.java
+++
@@ -1,8 +0,0 @@
-package org.micro.neural.extension.singleton;
-
-import org.micro.neural.extension.SPI;
-
-@SPI(single = true)
-public interface NpiSingleton {
-	long spiHello();
-}
